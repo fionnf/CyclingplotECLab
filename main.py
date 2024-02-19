7c6ee5bbbf97a67a56c2188414437d6c5206c788
fionnf <36422860+fionnf@users.noreply.github.com>
2024-02-19 22:36:30 +0100

    add plotting fu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import eclabfiles as ecf
 import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 def process_mpr_data(mpr_file_path):
     df = ecf.to_df(mpr_file_path)
@@ -35,5 +37,36 @@ def process_mpr_data(mpr_file_path):
     return processed_df
 
 
+def plot_capacities_and_efficiency(mpr_file_path):
+    processed_df = process_mpr_data(mpr_file_path)
+
+    # Convert the 'Last_UTS' Unix timestamp to datetime
+    processed_df['Time_Days'] = (pd.to_datetime(processed_df['Last_UTS'], unit='s') - pd.to_datetime(
+        processed_df['Last_UTS'].iloc[0], unit='s')).dt.total_seconds() / (24 * 3600)
+
+    # Create subplots: one y-axis for capacities, another for Coulombic Efficiency
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # Plot Charge and Discharge Capacities
+    fig.add_trace(go.Scatter(x=processed_df['Cycle_Number'], y=processed_df['Charge_Capacity'], mode='markers',
+                             name='Charge Capacity', marker=dict(color='blue')), secondary_y=False)
+    fig.add_trace(go.Scatter(x=processed_df['Cycle_Number'], y=processed_df['Discharge_Capacity'], mode='markers',
+                             name='Discharge Capacity', marker=dict(color='red')), secondary_y=False)
+
+    # Plot Coulombic Efficiency on secondary y-axis
+    fig.add_trace(
+        go.Scatter(x=processed_df['Cycle_Number'], y=processed_df['Coulombic Efficiency'], mode='markers',
+                   name='Coulombic Efficiency', marker=dict(color='green')), secondary_y=True)
+
+    # Set x-axis title
+    fig.update_xaxes(title_text="Cycle Number")
+
+    # Set y-axes titles
+    fig.update_yaxes(title_text="Capacity (mAh)", secondary_y=False)
+    fig.update_yaxes(title_text="Coulombic Efficiency (%)", secondary_y=True)
+
+    fig.show()
+
+
 mpr_file_path = r'C:\Users\fionn\Desktop\AkkuSpin\Test\Cyclerfolder\FF025e_cycle.mpr'
-process_mpr_data(mpr_file_path)
+plot_capacities_and_efficiency(mpr_file_path)
